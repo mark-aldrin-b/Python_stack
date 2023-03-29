8d1dfa57f0d426cc8775f27638d8a3d79197a1d0
mark-aldrin-b <aldrin.bersabe@gmail.com>
2023-03-29 08:43:15 +0800

    UserBankAcount

--- python/fundamentals/oop/bersabe_markaldrin_Vs_bankAccount/bersabe_markaldrin_Vs_bankAccount.py
+++ python/fundamentals/oop/bersabe_markaldrin_Vs_bankAccount/bersabe_markaldrin_Vs_bankAccount.py
@@ -26,12 +26,13 @@ class BankAccount:
     
     def yield_interest(self):
         if self.balance > 0:
-            self.balance = self.balance * self.interest_rate
+            self.balance = self.balance + (self.balance * self.interest_rate)
         return self
     
     @classmethod
     def get_instances_bank(cls):
-        print(f"Bank name: {cls.bank_name}, All accounts:{cls.all_accounts}")
+        for account in cls.all_accounts:
+            account.display_account_info()
             
 
 aldrin = BankAccount(0.05, 0)
@@ -42,3 +43,47 @@ aldrin.deposit(500).deposit(500).deposit(500).yield_interest().display_account_i
 bolt.deposit(1000).deposit(1000).withdraw(500).withdraw(500).withdraw(500).withdraw(500).yield_interest().display_account_info()
 
 BankAccount.get_instances_bank()
+"""
+class BankAccount:
+    accounts = []
+    def __init__(self,int_rate,balance):
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.accounts.append(self)
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self,amount):
+        if(self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+    
+    def display_account_info(self):
+        print(f"Balance: {self.balance}")
+        return self
+
+    def yeild_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
+
+savings = BankAccount(.05,1000)
+checking = BankAccount(.02,5000)
+
+savings.deposit(10).deposit(20).deposit(40).withdraw(600).yeild_interest().display_account_info()
+checking.deposit(100).deposit(200).deposit(400).withdraw(60).yeild_interest().display_account_info()
+
+BankAccount.print_all_accounts() 
+
+"""
